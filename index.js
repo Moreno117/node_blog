1634c4c5c6e59facbde6ebf4f83a36c2b2ad4c50
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-02 00:02:20 -0600

    Add moongose

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://localhost:27017/node-blog");
+// Use static assets
+app.use(express.static("public"));
 
+// Connect local DB
+mongoose.connect("mongodb://localhost:27017/node-blog");
+
+// Setting up Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
