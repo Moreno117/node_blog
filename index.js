edbbfcd7964d5b12728507517ab5e2a016ca8f00
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-02 19:03:37 -0600

    Add post and routes for create new post

--- index.js
+++ index.js
@@ -5,21 +5,64 @@ var mongoose = require("mongoose");
 
 // Use static assets
 app.use(express.static("public"));
-
-// Connect local DB
-mongoose.connect("mongodb://localhost:27017/node-blog");
-
 // Setting up Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Connect local DB
+mongoose.connect("mongodb://localhost:27017/node-blog");
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    content: String
+});
+
+var Post = mongoose.model("Post", postSchema);
+
 
 app.get('/', (req, res) => {
     res.render('landing.ejs');
 });
 
 app.get('/blog', (req, res) => {
-    res.render('blog.ejs');
+    Post.find({}, (err, posts) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render('blog.ejs', { posts: posts });
+        }
+    });
+});
+
+app.get("/post/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.get("/post/:id", (req, res) => {
+    Post.findById(req.params.id, (err, postFound) => {
+        if (err) {
+            console.log("There was an error", err)
+        } else {
+            res.render("show.ejs", { postFound: postFound });
+        }
+    })
 })
+
+app.post("/post", (req,res) => {
+    const { title, image, content } = req.body;
+    const userPost = {
+        title: title,
+        image: image,
+        content: content
+    }
+    Post.create(userPost, (err, newPost) => {
+        if(err){
+            console.log(err);
+        }else {
+            res.redirect("/blog")
+        }
+    });
+});
 
 app.listen(3000, () => {
     console.log('App listen on port 3000');
