e2641c1e5858c8b2d43495fa405675640780cc08
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-17 22:27:58 -0600

    Update utils settings

--- util/index.js
+++ util/index.js
@@ -1,5 +1,12 @@
-const INSTAGRAM_TOKEN = "5459874.a4f57a2.a68491206c7e454d89e05e5d9f513e21"; 
+const INSTAGRAM_TOKEN = process.env.INSTAGRAM_TOKEN; 
+
+const CLOUDINARY_SETTINGS = {
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: process.env.CLOUD_API_SECRET
+};
 
 module.exports = {
+    CLOUDINARY: CLOUDINARY_SETTINGS,
     INSTAGRAM_TOKEN: INSTAGRAM_TOKEN
-};
+}
