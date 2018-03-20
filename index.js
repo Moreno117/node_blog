c9c8924c470da2d0cac58c34efa095bbfe3cd058
Carlos <jmoreno@elfinanciero.com.mx>
2018-03-20 14:06:15 -0600

    Add pagination landing blog

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require("express"),
-    methodOverride = require("method-override"),
-    LocalStrategy = require("passport-local"),
-    bodyParser = require("body-parser"),
-    cloudinary = require('cloudinary'),
-    User = require("./models/user"),
-    Post = require("./models/post"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    _ = require("lodash"),
-    app = express();
+  methodOverride = require("method-override"),
+  LocalStrategy = require("passport-local"),
+  bodyParser = require("body-parser"),
+  cloudinary = require("cloudinary"),
+  User = require("./models/user"),
+  Post = require("./models/post"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  _ = require("lodash"),
+  app = express();
 
 const authRoutes = require("./routes/auth"),
-    postRoutes = require("./routes/posts"),
-    socialRoutes = require("./routes/social");
+  postRoutes = require("./routes/posts"),
+  socialRoutes = require("./routes/social");
 
 const APIS = require("./util");
 
 // Helper for parse HTML
-app.locals.htmlParsed = html => _.escape(html).replace(/\n/g, '<br>');
+app.locals.htmlParsed = html => _.escape(html).replace(/\n/g, "<br>");
 // Use static assets
 app.use(express.static("public"));
 // Setting up Body Parser
@@ -34,11 +34,13 @@ mongoose.connect(url);
 cloudinary.config(APIS.CLOUDINARY);
 
 // Passport configuration
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Blog of the year 2018",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -46,25 +48,36 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Spread id on the routes
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    next();
-})
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", authRoutes);
 app.use("/post", postRoutes);
 app.use("/social", socialRoutes);
 
-app.get('/blog', (req, res) => {
-    Post.find({}, (err, posts) => {
-        if(err){
-            console.log(err);
-        } else {
-            res.render('blog/blog.ejs', { posts: posts });
-        }
-    });
+app.get("/blog", (req, res) => {
+  Post.paginate({}, { page: 2, limit: 3 }, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blog/blog.ejs", { posts: posts.docs });
+    }
+  });
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
-    console.log('App listen on port 3000');
+app.get("/page", (req, res) => {
+  Post.paginate({}, { page: 2, limit: 3 }, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+      res.send("ok");
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log("App listen on port 3000");
 });
--- models/post.js
+++ models/post.js
@@ -1,11 +1,14 @@
 var mongoose = require("mongoose");
+var mongoosePaginate = require("mongoose-paginate");
 
 var postSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    content: String,
-    resume: String
+  title: String,
+  image: String,
+  content: String,
+  resume: String
 });
+
+postSchema.plugin(mongoosePaginate);
 
 var Post = mongoose.model("Post", postSchema);
 
